0c81cc514c5f6dfe2e22c6b8e8f380799fea80d5
ixilminiussi <ixil.miniussi@outlook.fr>
2025-02-26 09:52:35 +0100

    [Fix] ImGui Math issues

--- src/Mark.hpp
+++ src/Mark.hpp
@@ -3,24 +3,24 @@
 #include "ColorRGBA.hpp"
 #include "RelativePosition.hpp"
 
-namespace ImGG {
+namespace ImGG
+{
 
-struct Mark {
+struct Mark
+{
     RelativePosition position;
-    ColorRGBA        color;
+    ColorRGBA color;
 
     Mark( // We need to explicitly define the constructor in order to compile with MacOS Clang in C++ 11
-        RelativePosition position = RelativePosition{0.f},
-        ColorRGBA        color    = {0.f, 0.f, 0.f, 1.f}
-    )
-        : position{position}
-        , color{color}
-    {}
-
-    friend auto operator==(const Mark& a, const Mark& b) -> bool
+        RelativePosition position = RelativePosition{0.f}, ColorRGBA color = {0.f, 0.f, 0.f, 1.f})
+        : position{position}, color{color}
     {
-        return a.position == b.position
-               && a.color == b.color;
+    }
+
+    friend auto operator==(const Mark &a, const Mark &b) -> bool
+    {
+        return a.position == b.position && a.color.x == b.color.x && a.color.y == b.color.y && a.color.z == b.color.z &&
+               a.color.w == b.color.w;
     };
 };
 
--- src/internal.hpp
+++ src/internal.hpp
@@ -3,7 +3,10 @@
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include <imgui.h>
 
-namespace ImGG { namespace internal {
+namespace ImGG
+{
+namespace internal
+{
 
 inline auto line_height() -> float
 {
@@ -17,7 +20,7 @@ inline auto button_size() -> ImVec2
 
 inline auto gradient_position(float x_offset) -> ImVec2
 {
-    return ImGui::GetCursorScreenPos() + ImVec2(x_offset, 0.f);
+    return {ImGui::GetCursorScreenPos().x + x_offset, ImGui::GetCursorScreenPos().y};
 }
 
 inline auto border_color() -> ImU32
@@ -25,4 +28,5 @@ inline auto border_color() -> ImU32
     return ImGui::GetColorU32(ImGuiCol_Border);
 }
 
-}} // namespace ImGG::internal
+} // namespace internal
+} // namespace ImGG
